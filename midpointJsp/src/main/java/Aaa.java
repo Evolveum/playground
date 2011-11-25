5d70ee1b7ffe0bad975b6f15161bfaf3b340a65f
Michal Serbak <michal.serbak@evolveum.com>
2011-11-25 16:00:48 +0000

    midpoinJsp maven project.

---
+++ midpointJsp/src/main/java/Aaa.java
@@ -0,0 +1,17 @@
+public class Aaa {
+	public Object getObjectB() {
+		Bbb objectB = new Bbb();
+		return objectB;
+	}
+
+	public Object getObjectC() {
+		Ccc objectC = new Ccc();
+		return objectC;
+	}
+
+	public Object getXmlDocument(String source) {
+		//XmlParser xmlParser = new XmlParser(source);
+		//return xmlParser.getXmlDocument();
+		return null;
+	}
+}
---
+++ midpointJsp/src/main/java/Ccc.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ccc {
+	public int count = 456789;
+	public String variable = "World";
+	public List<String> list = new ArrayList<String>();
+	public String html = "";
+
+	public Ccc() {
+		list.add("Janko");
+		list.add("Hraško");
+		list.add("Jožko");
+		list.add("Mrkvièka");
+		html = "<table width='300px' border='2'>" + createRowsAndCols(4, 3) + "</table>";
+	}
+
+	private String createRowsAndCols(int rowsCount, int colsCount) {
+		int counter = rowsCount*colsCount;
+		String result = "";
+		for (int i = 0; i < rowsCount; i++) {
+			result += "<tr>";
+			for (int j = 0; j < colsCount; j++) {
+				result += "<td>" + counter + "</td>";
+				counter--;
+			}
+			result += "</tr>";
+		}
+		return result;
+	}
+
+}
