96a87c35972fcfe5834e257a31c29f30114a3ad1
Viliam Repan <viliam.repan@evolveum.com>
2011-11-29 19:13:05 +0000

    wicket prototype spring security integration

--- wicket-prototype/src/main/java/com/evolveum/midpoint/web/component/xml/ace/mode-xml.js
+++ wicket-prototype/src/main/java/com/evolveum/midpoint/web/component/xml/ace/mode-xml.js
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2011 Evolveum
+ *
+ * The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at
+ * http://www.opensource.org/licenses/cddl1 or
+ * CDDLv1.0.txt file in the source code distribution.
+ * See the License for the specific language governing
+ * permission and limitations under the License.
+ *
+ * If applicable, add the following below the CDDL Header,
+ * with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ *
+ * Portions Copyrighted 2011 [name of copyright owner]
+ */
+
 define("ace/mode/xml", ["require","exports","module","pilot/oop","ace/mode/text","ace/tokenizer","ace/mode/xml_highlight_rules","ace/mode/behaviour/xml"], function(a, b, c) {
     var d = a("pilot/oop"),e = a("ace/mode/text").Mode,f = a("ace/tokenizer").Tokenizer,g = a("ace/mode/xml_highlight_rules").XmlHighlightRules,h = a("ace/mode/behaviour/xml").XmlBehaviour,i = function() {
         this.$tokenizer = new f((new g).getRules()),this.$behaviour = new h
