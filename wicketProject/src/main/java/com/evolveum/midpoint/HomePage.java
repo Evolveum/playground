e59265941315f22beccc276adce8ca0ece0b5181
Michal Serbak <michal.serbak@evolveum.com>
2011-12-02 13:23:15 +0000

    added wicketProject for testing

---
+++ wicketProject/src/main/java/com/evolveum/midpoint/HomePage.java
@@ -0,0 +1,14 @@
+package com.evolveum.midpoint;
+
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+
+public class HomePage extends WebPage {
+
+	private static final long serialVersionUID = 1L;
+
+	public HomePage() {
+		add(new Label("message", "Hello World!"));
+	}
+
+}
