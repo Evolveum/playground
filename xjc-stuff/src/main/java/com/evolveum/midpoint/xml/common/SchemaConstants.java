5ad71a2462c684a95bc9180ff02c2fc0c6f11780
Viliam Repan <viliam.repan@evolveum.com>
2012-01-18 15:19:05 +0000

    xjc classes cleanup

--- xjc-stuff/src/main/java/com/evolveum/midpoint/xml/common/SchemaConstants.java
+++
@@ -1,9 +0,0 @@
-package com.evolveum.midpoint.xml.common;
-
-/**
- * @author lazyman
- */
-public class SchemaConstants {
-    
-    public static final String NS_COMMON = "http://midpoint.evolveum.com/xml/ns/public/common/common-1.xsd";
-}
