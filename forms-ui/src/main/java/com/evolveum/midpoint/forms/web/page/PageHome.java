4ddf937a37e0fbc5e31b59df0eeeeacbe83bc09b
Viliam Repan <viliam.repan@evolveum.com>
2012-07-06 13:39:40 +0000

    improved form ui schema, still not finished yet. Basic components for structured form. MID-790

--- forms-ui/src/main/java/com/evolveum/midpoint/forms/web/page/PageHome.java
+++ forms-ui/src/main/java/com/evolveum/midpoint/forms/web/page/PageHome.java
@@ -1,7 +1,11 @@
 package com.evolveum.midpoint.forms.web.page;
 
+import com.evolveum.midpoint.forms.web.forms.FormModel;
+import com.evolveum.midpoint.forms.web.forms.StructuredForm;
 import org.apache.wicket.devutils.debugbar.DebugBar;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.model.LoadableDetachableModel;
 
 /**
  * @author lazyman
@@ -16,6 +20,16 @@ public class PageHome extends WebPage {
         DebugBar debugPanel = new DebugBar("debugPanel");
         add(debugPanel);
 
+        Form mainForm = new Form("mainForm");
+        add(mainForm);
 
+        StructuredForm form = new StructuredForm("form", new LoadableDetachableModel<FormModel>() {
+
+            @Override
+            protected FormModel load() {
+                return null;  //To change body of implemented methods use File | Settings | File Templates.
+            }
+        });
+        mainForm.add(form);
     }
 }
