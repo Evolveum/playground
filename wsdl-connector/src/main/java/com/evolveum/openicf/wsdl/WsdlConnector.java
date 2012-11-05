ea67190213570dcc1c4d191fa5f4226d87e5f27c
Viliam Repan <viliam.repan@evolveum.com>
2012-11-05 18:13:27 +0000

    preparing wsdl connector, not finished yet.

---
+++ wsdl-connector/src/main/java/com/evolveum/openicf/wsdl/WsdlConnector.java
@@ -0,0 +1,11 @@
+package com.evolveum.openicf.wsdl;
+
+import com.evolveum.midpoint.xml.ns._public.wsdl_connector_1_wsdl.WsdlConnectorPortType;
+
+/**
+ * @author lazyman
+ */
+public class WsdlConnector {
+
+    private WsdlConnectorPortType service;
+}
