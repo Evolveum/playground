e3b19c8e9ee7a69899ff64f07767ee5da9a30236
Tadeas Chrapovic <49787603+tchrapovic@users.noreply.github.com>
2021-08-04 13:25:17 +0200

    Delete id-match-client/src/main/java/auth directory

--- id-match-client/src/main/java/auth/Authentication.java
+++
@@ -1,9 +0,0 @@
-package auth;
-
-
-import java.util.List;
-import java.util.Map;
-
-public interface Authentication {
-    void applyToParams(List<String> queryParams, Map<String, String> headerParams);
-}
