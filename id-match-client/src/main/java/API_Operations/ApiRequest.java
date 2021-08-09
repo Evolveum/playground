7480fea1341e0878e02f9d8ca2b6bb8fbb1e8a0f
Tadeas Chrapovic <49787603+tchrapovic@users.noreply.github.com>
2021-08-09 15:02:04 +0200

    Delete id-match-client/src/main/java/API_Operations directory

--- id-match-client/src/main/java/API_Operations/ApiRequest.java
+++
@@ -1,9 +0,0 @@
-package API_Operations;
-
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.ProtocolException;
-
-public interface ApiRequest {
-     void doRequest(HttpURLConnection httpURLConnection, String jsonString) throws IOException;
-}
